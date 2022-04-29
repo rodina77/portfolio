4e3b3b34305077fba3642a2481f81d953500c7f3
Rodina Ibrahim <99216903+rodina77@users.noreply.github.com>
2022-04-29 04:27:48 +0200

    version1

---
+++ js/index.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    $("#btn2").click(function(){
+      $(".flox4").show(3000);
+      $("#btn2").hide(3000);
+    });
+    $("#btn3").click(function(){
+        $(".flox4").hide(3000);
+        $("#btn2").show(3000);
+      });
+  });
+
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
+  function reveal() {
+   var reveals = document.querySelectorAll(".reveal");
+ 
+   for (var i = 0; i < reveals.length; i++) {
+     var windowHeight = window.innerHeight;
+     var elementTop = reveals[i].getBoundingClientRect().top;
+     var elementVisible = 150;
+ 
+     if (elementTop < windowHeight - elementVisible) {
+       reveals[i].classList.add("active");
+     } else {
+       reveals[i].classList.remove("active");
+     }
+   }
+ }
+ window.addEventListener("animationstart",reveal);
+ 
